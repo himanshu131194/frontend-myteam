375f143a2efbdd399e3874d02141cef129c55d33
Himanshu <himanshu131194@gmail.com>
2022-03-01 16:54:02 +0530

    accounts screens

--- src/client/MainRouter.tsx
+++ src/client/MainRouter.tsx
@@ -2,20 +2,34 @@ import React from "react";
 import {
     BrowserRouter,
     Routes,
-    Route
+    Route,
+    Outlet,
+    Navigate
 } from "react-router-dom";
 // import App from './App';
 import {routes, RoutesType} from './Routes';
+import UsersContainer from "./containers/UsersContainer";
 
 interface Props { }
 
 interface States { }
+
+
+function PrivateOutlet(){
+    const isAuthenticaed: boolean = true;
+    return isAuthenticaed ? <Outlet/>: <Navigate to="/account/login" />
+}
+
+
 
 class MainRouter extends React.Component<Props, States> {
     render(): any {
         return (
             <BrowserRouter>
                 <Routes>
+                    <Route path={'/'} element={<PrivateOutlet/>}>
+                        <Route path={'users'} element={<UsersContainer/>}/>                      
+                    </Route>
                     {routes.map((route: RoutesType)=>{
                         return <Route path={route.path} element={route.container} key={route.id}/>
                     })}
---
+++ src/client/containers/UsersContainer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+// import Accounts from '../components/Accounts';
+
+
+const UsersContainer: React.FC = ()=>{
+    return (
+        <h1>Users</h1>
+    )
+}
+
+export default UsersContainer;
